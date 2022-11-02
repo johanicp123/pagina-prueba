cd6226a711976b75bba8d58ee4b8783dfa3db955
johanicp123 <johanicp123@gmail.com>
2022-11-02 01:27:38 -0500

    actualizacion

--- prueba/src/App.tsx
+++ prueba/src/App.tsx
@@ -1,25 +1,22 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+    <body>
+      <div className="App , p-2">
+        <h5>HOT SALE -30% EN SANDALIAS</h5>
+      </div>
+      <div className="App2">
+          <ul>
+            <li>DIRECTORIO DE TIENDAS</li>
+            <li>SERVICIO AL CLIENTE</li>
+            <li>CUENTA</li>
+          </ul>
+      </div>
+      </body>
+    </>
   );
 }
 
